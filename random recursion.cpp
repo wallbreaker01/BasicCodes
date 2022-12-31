52d2524c49d30eaed725dda48f1c820064fb24e8
wallbreaker01 <107265135+wallbreaker01@users.noreply.github.com>
2022-12-31 21:17:04 +0600

    Create random recursion.cpp

---
+++ random recursion.cpp
@@ -0,0 +1,72 @@
+/*
+#include<iostream>
+using namespace std;
+ 
+int fibonacci(int n)
+{
+    if((n==1) || (n==0)) return(n);
+    else return(fibonacci(n-1)+fibonacci(n-2));
+}
+ 
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cout<<fibonacci(i)<<" ";
+    }
+}
+*/
+
+
+
+
+
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+void recursiveReverse(string &str, int i = 0)
+{
+    int n = str.length();
+    if (i == n / 2)
+        return;
+    swap(str[i], str[n - i - 1]);
+    recursiveReverse(str, i + 1);
+}
+
+int main()
+{
+    string str = "sowmikroy";
+    recursiveReverse(str);
+    cout << str;
+    return 0;
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
